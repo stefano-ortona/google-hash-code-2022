82d9a407e7a17b0de5619a315251e5a0bfd4f1aa
Giorgio27 <giopag27@gmail.com>
2022-02-24 21:02:09 +0100

    solutionContainer toString

--- src/main/java/google/com/ortona/hashcode/qualification/model/SolutionContainer.java
+++ src/main/java/google/com/ortona/hashcode/qualification/model/SolutionContainer.java
@@ -2,6 +2,8 @@ package google.com.ortona.hashcode.qualification.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SolutionContainer {
 
@@ -19,6 +21,17 @@ public class SolutionContainer {
 
     @Override
     public String toString() {
-        return "__";
+    	StringBuilder output = new StringBuilder("" + SOLUTION_PROJECT_LIST.size()).append("\n");
+    	for(final Project project: SOLUTION_PROJECT_LIST) {
+    		output.append(project.getName()).append("\n");
+    		final List<Skill> skills = project.getSkillRequiredList();
+    		final Map<String,Person> contributors = project.getContributorList();
+    		final String names = skills.stream().
+    				map(skill -> contributors.get(skill.getName()).getName()).
+    				collect(Collectors.joining(" "));
+    		output.append(names).append("\n");
+    	}
+    	
+        return output.toString();
     }
 }
