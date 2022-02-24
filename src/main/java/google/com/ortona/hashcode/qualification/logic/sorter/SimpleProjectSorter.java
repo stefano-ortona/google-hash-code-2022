3e49f96a59d3b4dedc03157065f9503334944ee4
carlo luchessa <c.luchessa@reply.it>
2022-02-24 20:40:49 +0100

    up.

--- src/main/java/google/com/ortona/hashcode/qualification/logic/sorter/SimpleProjectSorter.java
+++ src/main/java/google/com/ortona/hashcode/qualification/logic/sorter/SimpleProjectSorter.java
@@ -8,7 +8,7 @@ public class SimpleProjectSorter implements IProjectSorter {
 
     @Override
     public void sortProject(List<Project> projectList) {
-
+        projectList.sort(new SimpleProjectComparator());
     }
 
 }
